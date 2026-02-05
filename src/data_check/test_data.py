17200cee53904a3b77f4f9f5bf694daf42400f2b
dj <dj@Sireens-MacBook-Pro.local>
2026-02-05 08:58:51 -0500

    Finalizing pipeline for release 1.0.0

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -87,3 +87,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+def test_row_count(data):
+    """
+    Test if the dataset is an appropriate size
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data, min_price, max_price):
+    """
+    Test if the price is within the specified range
+    """
+    assert data['price'].between(min_price, max_price).all()
